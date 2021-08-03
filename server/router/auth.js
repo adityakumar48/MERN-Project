160b7d398a766713a60a6a1457c55b1aad2a7f26
Visinigiri Aditya <adityakumarverified@gmail.com>
2021-08-03 17:23:44 +0530

    valdations

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,60 +1,60 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
-require('../db/conn')
-const User  = require('../model/userSchema')
+require("../db/conn");
+const User = require("../model/userSchema");
 
-router.get('/', (req,res)=>{
-    res.send(`Hello World From The Sever`);
+router.get("/", (req, res) => {
+  res.send(`Hello World From The Sever`);
 });
 
-router.post('/register', async (req,res)=>{
-    const {name, email, phone, work, password,cpassword } = req.body;
+router.post("/register", async (req, res) => {
+  const { name, email, phone, work, password, cpassword } = req.body;
 
-    if(!name || !email || !phone || !work || !password || !cpassword){
-        return res.status(422).json({error:"Please Fill all Property"})
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res.status(422).json({ error: "Please Fill all Property" });
+  }
+
+  try {
+    const userExist = await User.findOne({ email: email });
+    if (userExist) {
+      return res.status(422).json({ error: "Email Already Exist" });
+    } else if (password != cpassword) {
+      return res.status(422).json({ error: "password Are not Matching" });
+    } else {
+      const user = new User({ name, email, phone, work, password, cpassword });
+
+      await user.save();
+      res.status(201).json({ message: "User Registered Succesfully" });
     }
-   
-    try{
-        const userExist = await User.findOne({email:email});
-        if(userExist){
-            return res.status(422).json({error:"Email Already Exist"})
-        }else if(password != cpassword){
-            return res.status(422).json({error:"password Are not Matching"})
-        }else{
+  } catch (err) {
+    console.log(err);
+  }
+});
 
+router.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ error: "Please Fill Vaild Details " });
+    }
 
-        const user = new User({name, email, phone, work, password,cpassword})
+    const userLogin = await User.findOne({ email: email });
+    if(userLogin){
+        const isMatch = await bcrypt.compare(password, userLogin.password)
 
-       await user.save();
-       res.status(201).json({message:"User Registered Succesfully"})
+        if (!isMatch) {
+          res.status(400).json({ error: "Invalid Credientials " });
+        } else {
+          res.json({ message: "user Signin Successfully" });
         }
-    } catch(err){
-        console.log(err)
+    }else {
+        res.status(400).json({ error: "Invalid Credientials" });
     }
-})
-
-router.post('/signin',async (req,res)=>{
-    
-
-    try{
-        const { email, password } = req.body;
-        if(!email || !password ){
-            return res.status(400).json({error:"Please Fill Vaild Details "})
-        }
-
-        const userLogin = await User.findOne({email:email});
-
-        !userLogin?res.status(400).json({error:"Please Enter Vaild Details "}):res.json({message:"user Signin Successfully"})
-
-    
-    }catch(err){
-        console.log(err)
-    }
-
-
-    
-    
-})
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
